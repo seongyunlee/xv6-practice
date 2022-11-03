72e179f8652a33d42a149a77e1821c215b1dbfcb
seongyunlee <priavate.seongyun@gmail.com>
2022-11-03 13:03:46 +0900

    no iterrupt

--- mytest.c
+++ mytest.c
@@ -8,9 +8,8 @@ int main(){
     uint i = (uint) mmap(0, 8192, PROT_READ, MAP_POPULATE|MAP_ANONYMOUS, -1, 0);
     printf(1,"%d %x",*((int *)i));
     int* x = (int *) map(8192,4096,PROT_READ|PROT_WRITE,MAP_ANONYMOUS,-1,0);
-    int i=0;
-    for(;x<1024;x++){
-        x[0]=i++;
+    for(int i=0;i<1024;i++){
+        x[i]=i;
     }
     for(int k=0;k<1024;k++){
         printf(1,"%d",x[k]);
