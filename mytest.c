985924c29b0c8785e09a2e5ee82b5ee2e3f1f2c5
seongyunlee <priavate.seongyun@gmail.com>
2022-11-06 22:36:36 +0900

    no iterrupt

--- mytest.c
+++ mytest.c
@@ -14,6 +14,17 @@ int main(){
     for(int k=0;k<1024;k++){
         printf(1,"%d",x[k]);
     }
+
+    if((fd = open("a.txt", 0))==0){
+        printf(1,"fail/n");
+    }
+    else{
+        printf(1,"success/n");
+        char buf[26];
+        int len=read(fd,buf,26);
+        printf(1,"%s/n",buf);
+    }
+
     printf(1,"\n");
     exit();
 }
