a5d83c68c345ff65494fc2a6d0996bf9a89b9514
seongyunlee <priavate.seongyun@gmail.com>
2022-11-06 23:46:34 +0900

    handler and test

--- vm.c
+++ vm.c
@@ -7,6 +7,9 @@
 #include "proc.h"
 #include "elf.h"
 #include "file.h"
+#include "fs.h"
+#include "spinlock.h"
+#include "sleeplock.h"
 
 extern char data[];  // defined by kernel.ld
 pde_t *kpgdir;  // for use in scheduler()
