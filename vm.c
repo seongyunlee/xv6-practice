41c56f48df3131d1b2c94148d69113c74b42832f
seongyunlee <priavate.seongyun@gmail.com>
2022-11-07 00:57:20 +0900

    handler and test

--- vm.c
+++ vm.c
@@ -391,7 +391,7 @@ copyout(pde_t *pgdir, uint va, void *p, uint len)
 }
 uint mmap_fileread(struct file *f,uint va,int offset,int size){
   ilock(f->ip);
-  cprintf("mmapfile readi at %d",offset)
+  cprintf("mmapfile readi at %x",(int)va);
   int r_byte = readi(f->ip,(char *)va,(uint)offset,10);
   offset+=r_byte;
   iunlock(f->ip);
