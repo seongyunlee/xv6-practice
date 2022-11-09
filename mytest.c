9f08fdbfc3330ddc4c86e13f49e2b0af91e4d7c5
seongyunlee <priavate.seongyun@gmail.com>
2022-11-09 21:27:58 +0900

    finals before deleting annotation

--- mytest.c
+++ mytest.c
@@ -44,6 +44,7 @@ int main(){
     }
     printf(1,"\n\n munmap test\n");
     printf(1,"this massage should be printed %c %x\n",mmap_addr[0],(int)mmap_addr);
+    mmap_addr[0]=3;
     munmap((uint)mmap_addr);
     printf(1,"free space after unmmap %d\n",freemem());
     printf(1,"this massage should not be printed %c %x\n",mmap_addr[0],(int)mmap_addr);
