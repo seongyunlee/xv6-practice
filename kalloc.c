48a3353a1952746eccaa0c1f10330b2b18bd2e17
seongyunlee <priavate.seongyun@gmail.com>
2022-11-09 10:11:27 +0900

    fork edit define revised

--- kalloc.c
+++ kalloc.c
@@ -72,7 +72,7 @@ kfree(char *v)
   r->next = kmem.freelist;
   kmem.freelist = r;
 
-  cprintf("kfree pa %x->next = %x",(int)v,(int)v->next);
+  cprintf("kfree pa %x->next = %x",(int)v,(int)r->next);
   if(kmem.use_lock)
     release(&kmem.lock);
 }
