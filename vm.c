12314f160f52b1c721bb95b44aef82cd024f8d2e
seongyunlee <priavate.seongyun@gmail.com>
2022-11-09 21:09:07 +0900

    finals before deleting annotation

--- vm.c
+++ vm.c
@@ -353,14 +353,14 @@ copyuvm(pde_t *pgdir, uint sz)
     if(ma->p!=myproc()) continue;
     //copy only the allocated mmap
     pte = walkpgdir(pgdir, (void *) ma->addr+MMAPBASE, 0);
-    if(pte ==0 || (*pte&PTE_P)) continue;
+    if(pte ==0 || (pte!=0 && *pte&PTE_P>0)) continue;
     for(i = ma->addr+MMAPBASE; i < ma->addr+ma->length+MMAPBASE; i += PGSIZE){
       pa = PTE_ADDR(*pte);
       flags = PTE_FLAGS(*pte);
       if((mem = kalloc()) == 0)
         goto bad;
       memmove(mem, (char*)P2V(pa), PGSIZE);
-      cprintf("copy %x %x \n",(int)i,V2P(mem));
+      cprintf("copy %x %x \n",(int)i,mem);
       if(mappages(d, (void*)i, PGSIZE, V2P(mem), flags) < 0) {
         kfree(mem);
         goto bad;
